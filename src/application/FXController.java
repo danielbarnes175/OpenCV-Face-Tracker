bf8b3b16d6b3fe2bc4dc99b254e78592eff816be
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2018-10-20 02:30:11 -0500

    Add functionality for Video input, with bugs

--- src/application/FXController.java
+++ src/application/FXController.java
@@ -1,5 +1,102 @@
 package application;
 
-public class FXController {
+import java.io.ByteArrayInputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import utilities.Utils;
+
+public class FXController {
+	@FXML
+	private Button button;
+	@FXML
+	private ImageView currentFrame;
+	
+	private ScheduledExecutorService timer;
+	private VideoCapture capture = new VideoCapture();
+	private boolean isActive = false;
+	private static int cameraID = 0;
+	
+	@FXML
+	protected void startCamera(ActionEvent event) {
+		if (!isActive) {
+		this.capture.open(cameraID);
+		if (this.capture.isOpened()) {
+		Runnable frameGrabber = new Runnable() {
+
+			@Override
+			public void run() {
+				Mat frame = grabFrame();
+				Image imageToShow = Utils.mat2Image(frame);
+				updateImageView(currentFrame, imageToShow);
+			}};
+		
+		this.timer = Executors.newSingleThreadScheduledExecutor();
+		this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+		
+		this.button.setText("Stop");
+		}
+		else {
+			System.err.println("Can't open camera connection.");
+		}
+	}
+		else {
+			isActive = false;
+			this.button.setText("Start Camera");
+			this.stopAcquisition();
+		}
+}
+	
+	protected void updateImageView(ImageView view, Image image) {
+		// TODO Auto-generated method stub
+		Utils.onFXThread(view.imageProperty(), image);
+	}
+
+	private void stopAcquisition() {
+		// TODO Auto-generated method stub
+		try {
+			this.timer.shutdown();
+			this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+			} catch (InterruptedException e) {
+				System.err.println("Exception while stopping frame capture" + e);
+			}
+			if (this.capture.isOpened()) {
+				this.capture.release();
+			}
+	}
+
+	protected Mat grabFrame() {
+		Mat frame = new Mat();
+		if (capture.isOpened()) {
+		try {
+		this.capture.read(frame);
+		if (!frame.empty()) {
+			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+			}
+		
+		} catch(Exception e) {
+			System.err.println("grabFrame gets an exception: " + e);
+			}
+		}
+		return frame;
+	}
+	
+	protected void setClosed() {
+		this.stopAcquisition();
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,54 @@
 package application;
 	
+
+import org.opencv.core.Core;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Facial Tracking FX.fxml"));
-			Scene scene = new Scene(root,400,400);
+			
+			// load the FXML resource
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Facial Tracking FX.fxml"));
+			// store the root element so that the controllers can use it
+			BorderPane rootElement = (BorderPane) loader.load();
+			// create and style a scene
+			Scene scene = new Scene(rootElement, 800, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// create the stage with the given title and the previously created
+			// scene
+			primaryStage.setTitle("Big Brother is Watching");
 			primaryStage.setScene(scene);
+			// show the GUI
 			primaryStage.show();
+			
+			// set the proper behavior on closing the application
+			FXController controller = loader.getController();
+			primaryStage.setOnCloseRequest((new EventHandler<WindowEvent>() {
+				public void handle(WindowEvent we)
+				{
+					controller.setClosed();
+				}
+			}));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
+		
 		launch(args);
 	}
+
 }
